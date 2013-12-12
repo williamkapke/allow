82788466a06252c1c25e6b36db64774adf5e9c9a
William Wicks <wjwicks@gmail.com>
2013-12-12 22:15:18 +0000

    convert to mocha tests

--- tests.js
+++ tests.js
@@ -1,8 +1,6 @@
-
-
-var vows = require('vows'),
-	assert = require('assert'),
-	Validator = require("./validator");
+var should=require("should");
+var assert = require('assert');
+var Validator = require("./validator");
 
 function length(min, max) { return function(value){
 	if(value.length<min) return "Too short";
@@ -12,291 +10,207 @@ function length(min, max) { return function(value){
 
 
 
-vows.describe("Validation tests")
-	.addBatch({
-		"Constructing a Validator": {
-			"without any params": {
-				topic: function(){
-					return new Validator()
-				},
-				"is pointless- but doable": function(v){
-					assert.isNotNull(v);
-					assert.equal(typeof v, 'function');
-				}
+describe("Constructing a Validator", function() {
+	describe("without any params", function() {
+		it("should be doable", function(){
+			var validate = new Validator();
+			validate.should.be.type('function');
+		})
+	});
+	describe("with a nested Validator", function() {
+		var validate = new Validator({
+			people: new Validator()
+		});
+		var result = validate("{people{name,phone,role}}", {people:{}});
+
+		it("should populate nested errors", function() {
+			result.should.have.property('errors');
+			result.errors.should.have.property('people');
+			result.errors.people.should.have.property('name');
+			result.errors.people.should.have.property('phone');
+			result.errors.people.should.have.property('role');
+		});
+	})
+});
+describe("Using propex '[]'", function() {
+	var validate = new Validator({
+		name: { test: function(value) { if(value != "roojoo") return "Booooo. Bad kitty." } }
+	});
+	var px = '[]';
+
+	describe("and try to validate against an Object", function() {
+		it("should return the default 'required' message", function() {
+			var result = validate(px, {});
+			result.errors.should.equal(Validator.errors.required());
+		});
+	});
+	describe("and try to validate an empty Array", function() {
+		it("should not return errors and valid should be empty", function() {
+			var result = validate(px, []);
+			result.should.not.have.property('errors');
+			result.should.have.property('valid');
+			result.valid.should.be.instanceof(Array).and.have.lengthOf(0);
+		});
+	});
+	describe("and try to validate a simple Array", function() {
+		it("should cause an error", function() {
+			(function(){ validate(px, [1,2,3]); }).should.throw();
+		});
+	});
+});
+describe("Using propex '{}'", function() {
+	var validate = new Validator({
+		name: { test: function(value) { if(value != "roojoo") return "Booooo. Bad kitty." } }
+	});
+	var px = '{}';
+
+	describe("and trying to validate against an Object", function() {
+		it("should not return errors and valid should be empty", function() {
+			var result = validate(px, {});
+			result.should.not.have.property('errors');
+			result.valid.should.be.instanceof(Object);
+			result.valid.should.be.empty;
+		});
+	});
+	describe("and trying to validate against an Array", function() {
+		it("should return the default 'required' message", function() {
+			var result = validate(px, []);
+			result.errors.should.equal(Validator.errors.required());
+		});
+	});
+	describe("validating an object with extra properties", function() {
+		it("should ignore properties that are not in the propex", function() {
+			var result = validate(px, {x:1});
+			result.should.not.have.property('errors');
+			result.should.have.property('valid');
+			result.valid.should.be.instanceof(Object);
+			result.valid.should.be.empty;
+		});
+	});
+});
+describe("Using propex '{name,type}'", function() {
+	var validate = new Validator({
+		name: { test: function(value) { if(value != "roojoo") return "Booooo. Bad kitty." } }
+	});
+	var px = '{name,type}';
+
+	describe("and trying to validate against 'undefined'", function() {
+		it("should return the default 'required' message", function() {
+			var result = validate(px, undefined);
+			result.errors.should.equal(Validator.errors.required());
+		});
+	});
+	describe("and trying to validate against an empty Object", function() {
+		it("should return errors for all required children", function() {
+			var result = validate(px, {});
+			result.should.have.property('errors');
+			result.errors.should.have.property('name');
+			result.errors.should.have.property('type');
+		});
+	});
+
+	describe("and trying to validate against {name:'lace',type:'cat'}", function() {
+		it("should have an error because the name does not match 'roojoo'", function() {
+			var result = validate('{name,type}', {name:"lace",type:"cat"});
+			result.should.have.property('errors');
+			result.errors.name.should.eql('Booooo. Bad kitty.');
+		});
+	});
+	describe("and trying to validate against {name:'roojoo',type:'cat',xtra:'ignored'}", function() {
+		it("should successfully find all required items and ignore xtra", function() {
+			var result = validate(px, {name:'roojoo',type:'cat',xtra:'ignored'});
+			result.should.not.have.property('errors');
+			result.valid.should.eql({name:'roojoo',type:'cat'});
+		});
+	});
+	describe("and trying to validate against {name:'roojoo'}", function() {
+		it("should complain about the missing property", function() {
+			var result = validate(px, {name:"roojoo"});
+			result.should.have.property('errors');
+			result.errors.type.should.eql('This information is required');
+		});
+	});
+});
+describe("Optional properites", function() {
+	var validate = new Validator({
+		name: { test: function(value) { if(value != "roojoo") return "Booooo. Bad kitty." } }
+	});
+	var px = '{type?}';
+
+	describe("with missing data", function() {
+		var result = validate(px, {name:'roojoo'});
+
+		it("should not be in 'result.valid' object", function(){
+			result.valid.should.be.empty;
+		});
+		it("should not be in 'result.error' object", function(){
+			result.should.not.have.property('errors');
+		});
+	});
+});
+
+describe("Using sample Validator", function() {
+	var validate = new Validator({
+		"name": {
+			test: function(value){
+				if(value.length < 4) return "Dude, "+value+", your name is too short!";
 			},
-			"with a nested Validator": {
-				topic: function(){
-					return new Validator({
-						people: new Validator()
-					})
-				},
-				"should populate nested errors": {
-					topic: function(validate){
-						return validate("{people{name,phone,role}}", {people:{}});
-					},
-					"empty object should return errors for all required children": function(result){
-						assert.isDefined(result.errors);
-						assert.include(result.errors, 'people');
-						assert.isDefined(result.errors.people.name);
-						assert.isDefined(result.errors.people.phone);
-						assert.isDefined(result.errors.people.role);
-					}
+			set: function(model, value) {
+				model[this.name] = value+" is a suitable name.";
+			}
+		},
+		"nested": new Validator({
+			"something": {
+				test: function(value){
+					if(value.length < 4) return "no nonono";
 				}
 			}
-		}
-	})
-	.addBatch({
-		'Basic propex concepts: ': {
-			topic: function(){
-				return new Validator({
-					name: { test: function(value) { if(value != "roojoo") return "Booooo. Bad kitty." } }
-				});
-			},
-			"Propex:[]": {
-				"Data:{}": {
-					topic: function(validate){
-						return validate("[]", {});
-					},
-					"errors should equal the default 'required' message": function(result){
-						assert.equal(result.errors, Validator.errors.required());
-					}
-				},
-				"Data:[]": {
-					topic: function(validate){
-						return validate("[]", []);
-					},
-					"should not return errors and valid should be empty": function(result){
-						assert.isUndefined(result.errors);
-						assert.isArray(result.valid);
-						assert.isEmpty(result.valid);
-					}
-				},
-				"Data:[1,2,3]": {
-					topic: function(validate){
-						return validate("[]", [1,2,3]);
-					},
-					"throws error: needs to have subitems specified in propex": function(result){
-						assert.instanceOf(result, Error);
-					}
-				},
-				"noop": {}
-			},
-			"Propex:{}": {
-				"Data:{}": {
-					topic: function(validate){
-						return validate("{}", {});
-					},
-					"should not return errors and valid should be empty": function(result){
-						assert.isUndefined(result.errors);
-						assert.isObject(result.valid);
-						assert.isEmpty(result.valid);
-					}
-				},
-				"Data:[]": {
-					topic: function(validate){
-						return validate("{}", []);
-					},
-					"errors should equal the default 'required' message": function(result){
-						assert.equal(result.errors, Validator.errors.required());
-					}
-				},
-				'Data:{x:1,name:"roojoo"}': {
-					topic: function(validate){
-						return validate("{}", {x:1});
-					},
-					"should ignore properties that are not in the propex": function(result){
-						assert.isEmpty(result.valid);
-						assert.isUndefined(result.errors);
-					}
-				},
-				"bookend": {}
-			},
-			'Propex:{name,type}': {
-				'Data: undefined': {
-					topic: function(validate){
-						return validate('{name,type}', undefined);
-					},
-					"should return return string: 'This information is required'": function(result){
-						assert.isDefined(result.errors);
-						assert.isString(result.errors);
-						assert.equal(result.errors, 'This information is required');
-					}
-				},
-				'Data: {}': {
-					topic: function(validate){
-						return validate("{name,type}", {});
-					},
-					"should return errors for all required children": function(result){
-						assert.isDefined(result.errors);
-						assert.isDefined(result.errors.name);
-						assert.isDefined(result.errors.type);
-					}
-				},
-				'Data:{name:"lace",type:"cat"}': {
-					topic: function(validate){
-						return validate('{name,type}', {name:"lace",type:"cat",xtra:"ignored"});
-					},
-					"should have an error because the name does not match 'roojoo'": function(result){
-						assert.isObject(result.errors);
-						assert.includes(result.errors, 'name');
-					}
-				},
-				'Data:{name:"roojoo",type:"cat",xtra:"ignored"}': {
-					topic: function(validate){
-						return validate('{name,type}', {name:"roojoo",type:"cat",xtra:"ignored"});
-					},
-					"should successfully find all required items and ignore xtra": function(result){
-						assert.isUndefined(result.errors);
-						assert.isObject(result.valid);
-						assert.equal(result.valid.name, 'roojoo');
-						assert.equal(result.valid.type, 'cat');
-						assert.isUndefined(result.valid.xtra);
-					}
-				},
-				'Data:{name:"roojoo"}': {
-					topic: function(validate){
-						return validate('{name,type}', {name:"roojoo"});
-					},
-					"should complain about the missing property": function(result){
-						assert.isDefined(result.errors);
-						assert.equal(result.errors.type, 'This information is required');
-					}
-				},
-				"bookend": {}
-			},
-			'Optional property': {
-				'with missing data': {
-					topic: function(validate){
-						return validate('{type?}', {name:"roojoo"});
-					},
-					"should not be in 'result.valid' object": function(result){
-						assert.isEmpty(result.valid);
-					},
-					"should not be in 'result.error' object": function(result){
-						assert.isUndefined(result.errors);
-					}
-				},
-				"bookend": {}
-			},
-			"bookend": {}
-		}
-	})
-	.addBatch({
-		"Testing against sample Validator": {
-			topic: function(){
-				return new Validator({
-					"name": {
-						test: function(value){
-							if(value.length < 4) return "Dude, "+value+", your name is too short!";
-						},
-						set: function(model, value) {
-							model[this.name] = value+" is a suitable name.";
-						}
-					},
-					"nested": new Validator({
-						"something": {
-							test: function(value){
-								if(value.length < 4) return "no nonono";
-							}
-						}
-					})
-				});
-			},
-			"short name":{
-				topic: function(validate){
-					return validate("{name}", {name:"Ed"});
-				},
-				"should fail 'test' function": function(result){
-					assert.isEmpty(result.valid);
-					assert.include(result.errors, 'name');
-					assert.equal(result.errors.name, "Dude, Ed, your name is too short!");
-				}
-			},
-			"valid name":{
-				topic: function(validate){
-					return validate("{name}", {name:"Nicole"});
-				},
-				"calls 'set' and appends to the end": function(result){
-					assert.isUndefined(result.errors);
-					assert.include(result.valid, 'name');
-					assert.equal(result.valid.name, "Nicole is a suitable name.");
-				}
-			},
-			"with nested arrays":{
-				topic: function(validate){
-					return validate("{nested[{something}]}", {name:"Nicole",nested:[{something:"good"},{something:"bad"}]});
-				},
-				"should test array items and fail invalid items": function(result){
-					assert.isDefined(result.errors);
-					assert.isDefined(result.errors.nested);
-					assert.isDefined(result.errors.nested[1]);
-					assert.equal(result.errors.nested[1].something, "no nonono");
-				}
-			},
-			"with invalid nested items":{
-				topic: function(validate){
-					return validate("{nested{something}}", {name:"Nicole",nested:{something:"bad"}});
-				},
-				"should have errors for the invalid items": function(result){
-					assert.isDefined(result.errors);
-					assert.isDefined(result.errors.nested);
-					assert.equal(result.errors.nested.something, "no nonono");
-				}
-			},
-			"with valid nested items":{
-				topic: function(validate){
-					return validate("{nested{something}}", {name:"Nicole",nested:{something:"gooood"}});
-				},
-				"should not yield an error object": function(result){
-					assert.isUndefined(result.errors);
-				}
-			}
-		}
-	})
-	.run();
+		})
+	});
+
+	describe("test a short name", function() {
+		var result = validate("{name}", {name:"Ed"});
+
+		it("should fail 'test' function", function(){
+			result.valid.should.be.empty;
+			result.errors.should.have.property('name');
+			result.errors.name.should.eql("Dude, Ed, your name is too short!");
+		});
+	});
+	describe("test a valid name", function() {
+		var result = validate("{name}", {name:"Nicole"});
+
+		it("should call 'set'", function(){
+			result.should.not.have.property('errors');
+			result.valid.should.have.property('name');
+			result.valid.name.should.eql("Nicole is a suitable name.");
+		});
+	});
+	describe("test nested arrays", function() {
+		var result = validate("{nested[{something}]}", {name:"Nicole",nested:[{something:"good"},{something:"bad"}]});
+
+		it("should test array items and fail invalid items", function(){
+			result.should.have.property('errors');
+			result.errors.nested.should.have.property(1);
+			result.errors.nested[1].something.should.eql("no nonono");
+		});
+	});
+	describe("test invalid nested items", function() {
+		var result = validate("{nested{something}}", {name:"Nicole",nested:{something:"bad"}});
+
+		it("should have errors for the invalid items", function(){
+			result.should.have.property('errors');
+			result.errors.should.have.property('nested');
+			result.errors.nested.something.should.eql("no nonono");
+		});
+	});
+	describe("test valid nested items", function() {
+		var result = validate("{nested{something}}", {name:"Nicole",nested:{something:"gooood"}});
+
+		it("should not yield an error object", function(){
+			result.should.not.have.property('errors');
+		});
+	});
+});
 
 
-
-/*
- AssertionError
- fail
- ok
- equal
- notEqual
- deepEqual
- notDeepEqual
- strictEqual
- notStrictEqual
- throws
- doesNotThrow
- ifError
- match
- matches
- isTrue
- isFalse
- isZero
- isNotZero
- greater
- lesser
- inDelta
- include
- includes
- deepInclude
- deepIncludes
- isEmpty
- isNotEmpty
- lengthOf
- isArray
- isObject
- isNumber
- isBoolean
- isNaN
- isNull
- isNotNull
- isUndefined
- isDefined
- isString
- isFunction
- typeOf
- instanceOf
-*/
